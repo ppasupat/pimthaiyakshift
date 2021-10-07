851e83715cf5a547f9c5068e06013427a14f5df1
Panupong (Ice) Pasupat <panuponggong@gmail.com>
2021-10-06 21:41:19 -0700

    Added boilerplate

---
+++ main.js
@@ -0,0 +1,36 @@
+$(function () {
+  'use strict';
+
+  const SCREEN_WIDTH = 700, SCREEN_HEIGHT = 400;
+  const FRAME_RATE = 8;
+
+  // ################################
+  // Utilities
+
+  function gup(name) {
+    let regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+    let results = regex.exec(window.location.href);
+    return results === null ? "" : decodeURIComponent(results[1]);
+  }
+
+	// ################################
+  // Preloading and screen resizing
+
+  function resizeScreen() {
+    let ratio = Math.min(
+      1.0,
+      window.innerWidth / SCREEN_WIDTH,
+      (window.innerHeight - 25) / SCREEN_HEIGHT,
+    );
+    $('#game-wrapper').css({
+      'width': (SCREEN_WIDTH * ratio) + 'px',
+      'height': (SCREEN_HEIGHT * ratio) + 'px',
+    });
+    $('#game').css('transform', 'scale(' + ratio + ')');
+  }
+
+  resizeScreen();
+  $(window).resize(resizeScreen);
+
+
+});
